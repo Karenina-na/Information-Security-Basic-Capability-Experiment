a00de92cb641134b9fed046a9d53befd39b0f10d
Karenina-na <weizixiang0@outlook.com>
2023-04-01 21:49:05 +0800

    python第二部分第二题

--- Python实验/PythonEx/testCase/test_text_word.py
+++ Python实验/PythonEx/testCase/test_text_word.py
@@ -2,6 +2,7 @@
 # -*- coding: UTF-8 -*-
 
 import sys
+
 sys.path.append("../")
 
 import unittest
@@ -14,7 +15,8 @@ class TestWordFunc(unittest.TestCase):
     # 题目：词频统计
     def test_word_1(self):
         li = word_freq('./testData/text0.txt')
-        self.assertListEqual(li,[('b', 16), ('shorts', 4), ('ok', 4), ('dollars', 4), ('welcome', 2), ('twenty', 2), ('thanks', 2), ('purple', 2), ('nice', 2), ('need', 2)])
+        self.assertListEqual(li, [('b', 16), ('shorts', 4), ('ok', 4), ('dollars', 4), ('welcome', 2), ('twenty', 2),
+                                  ('thanks', 2), ('purple', 2), ('nice', 2), ('need', 2)])
 
 
 if __name__ == '__main__':
--- Python实验/PythonEx/textEx/text_hw.py
+++ Python实验/PythonEx/textEx/text_hw.py
@@ -4,12 +4,77 @@
 
 # 题目十一：摩斯码生成器
 def morse_code(usr_str):
-    pass
+    dict = {
+        'A': '. -',
+        'B': '- . . .',
+        'C': '- . - .',
+        'D': '- . .',
+        'E': '.',
+        'F': '. . - .',
+        'G': '- - .',
+        'H': '. . . .',
+        'I': '. .',
+        'J': '. - - -',
+        'K': '- . -',
+        'L': '. - . .',
+        'M': '- -',
+        'N': '- .',
+        'O': '- - -',
+        'P': '. - - .',
+        'Q': '- - . -',
+        'R': '. - .',
+        'S': '. . .',
+        'T': '-',
+        'U': '. . -',
+        'V': '. . . -',
+        'W': '. - -',
+        'X': '- . . -',
+        'Y': '- . - -',
+        'Z': '- - . .',
+        '0': '- - - - -',
+        '1': '. - - - -',
+        '2': '. . - - -',
+        '3': '. . . - -',
+        '4': '. . . . -',
+        '5': '. . . . .',
+        '6': '- . . . .',
+        '7': '- - . . .',
+        '8': '- - - . .',
+        '9': '- - - - .',
+    }
+    usr_str = usr_str.upper()
+    result = ''
+    for i in usr_str:
+        if i == ' ':
+            result += '    '
+        else:
+            result += dict[i] + '   '
+    return result.strip()
 
 
 # 题目十二：词频统计
 def word_freq(path):
-    pass
+    import re
+    with open("./testData/sight word.txt", 'r') as f:
+        sight_words = f.readline().split()
+    with open(path, 'r') as f:
+        lines = f.readlines()
+        words = []
+        for line in lines:
+            line = re.sub(r'[!~@#S%^&*0_\-+=8/?,.:"<V>]+', ' ', line)
+            words += line.split(' ')
+        words = [word.lower() for word in words]
+        freq = {}
+        for word in words:
+            word = word.strip()
+            if word in sight_words:
+                continue
+            if word not in freq and word != '' and word != '\n':
+                freq[word] = 1
+            elif word != '' and word != '\n':
+                freq[word] += 1
+    freq = sorted(freq.items(), key=lambda x: x[1], reverse=True)
+    return freq[:10]
 
 
 # 题目十四：敏感词过滤
@@ -20,6 +85,3 @@ def filter_words(user_input):
 if __name__ == '__main__':
     li = morse_code('I love Python')
     print(li)
-
-
-
